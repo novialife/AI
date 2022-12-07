f693a03125fc9970ade8488fa1f7ed89040de23a
mckaya <mckaya@kth.se>
2022-12-07 18:27:24 +0100

    Klar med rl2

--- RL/rl2/player_2.py
+++ RL/rl2/player_2.py
@@ -294,6 +294,7 @@ class PlayerControllerRandom(PlayerController):
         end_episode = False
         # ADD YOUR CODE SNIPPET BETWEEN EX. 1.2
         # Initialize a numpy array with ns state rows and na state columns with zeros
+        states = np.zeros((ns, na))
         # ADD YOUR CODE SNIPPET BETWEEN EX. 1.2
 
         while episode <= self.episode_max:
@@ -306,7 +307,8 @@ class PlayerControllerRandom(PlayerController):
 
                 # ADD YOUR CODE SNIPPET BETWEEN EX. 1.2
                 # Chose an action from all possible actions and add to the counter of actions per state
-                action = None
+                action = np.random.choice(possible_actions)
+                states[s_current, action] += 1
                 # ADD YOUR CODE SNIPPET BETWEEN EX. 1.2
 
                 action_str = self.action_list[action]
@@ -329,7 +331,7 @@ class PlayerControllerRandom(PlayerController):
             episode += 1
             end_episode = False
 
-        return n
+        return states
 
     def get_policy(self, Q):
         nan_max_actions_proxy = [None for _ in range(len(Q))]
